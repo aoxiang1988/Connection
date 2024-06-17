94d852521c8fa601287f7fb773c791be2cf1e927
bin yang <bin.yang@denso-ten.com>
2024-06-17 15:09:46 +0800

    no need model

--- automotive/src/main/java/com/example/myautoapplication/datamodel/ActionMode.java
+++
@@ -1,8 +0,0 @@
-package com.example.myautoapplication.datamodel;
-
-public class ActionMode {
-    public static final String AUDIO_LIST_READY = "audio list ready";
-    public static final String SERVICE_INTENT_ACTION = "service_action";
-    public static final String MEDIA_SOURCE_STOP_ACTION = "media_source_stop_action";
-    public static final String SERVICE_INTENT_INFO = "info";
-}
