6d2bb353b071a0a3fd6a7a8cfd1995b7a78d6fee
bin yang <bin.yang.tnrt@cn.denso.com>
2024-09-12 13:12:30 +0800

    fix test error

--- connecttoapilibrary/src/androidTest/java/com/sec/connecttoapilibrary/ExampleInstrumentedTest.java
+++ connecttoapilibrary/src/androidTest/java/com/sec/connecttoapilibrary/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
 package com.sec.connecttoapilibrary;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
         assertEquals("com.sec.connecttoapilibrary.test", appContext.getPackageName());
     }
